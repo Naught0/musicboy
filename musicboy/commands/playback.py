3bd1bcf3fa0ed8d1ffbf0e995db9e6f694e41f8b
James E <4589250+Naught0@users.noreply.github.com>
2024-10-28 23:25:22 -0400

    Separate commands

---
+++ musicboy/commands/playback.py
@@ -0,0 +1,35 @@
+from discord.ext import commands
+
+from musicboy.context import Context
+
+
+class Playback(commands.Cog):
+    @commands.command()
+    async def join(self, ctx: Context):
+        channel = ctx.author.voice.channel  # type: ignore
+        if channel is None:
+            return
+
+        await channel.connect()
+
+
+    @commands.command()
+    async def play(self, ctx: Context, url: str):
+        if ctx.voice_client is None:
+            try:
+                await ctx.author.voice.channel.connect()
+            except Exception:
+                return await ctx.message.add_reaction("❌")
+
+        if ctx.guild.voice_client.is_playing():
+            pass
+
+        ctx.voice_client.stop()
+        ctx.voice_client.play(discord.FFmpegPCMAudio())
+
+
+    @commands.command()
+    async def leave(self, ctx: Context):
+        ctx.bot
+        if ctx.voice_client is not None:
+            await ctx.voice_client.disconnect()
